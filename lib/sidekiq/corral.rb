ad4f2b635ccad4cd767a0a7f7f71c7d51c2f1354
Mo Lawson <mo@molawson.com>
2023-07-21 15:24:13 -0500

    Add client and server middleware
    Here's the basic functionality.
    
    Using the client middleware allows any job to be set to a given corral
    queue using Sidekiq::Job.set or by setting Sidekiq::Corral.current
    before enqueueing the job.
    
    Then the server middleware sets Sidekiq::Corral.current based on the
    corral for the job being processed, and that's used to put any jobs that
    are enqueued while the initial job is being processed into the same
    queue referenced in the corral.

--- lib/sidekiq/corral.rb
+++ lib/sidekiq/corral.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "sidekiq"
+
 require_relative "corral/version"
 
 module Sidekiq
@@ -13,6 +15,27 @@ module Sidekiq
 
     def self.current=(queue)
       Thread.current[:sidekiq_corral_queue] = queue
+    end
+
+    class Client
+      include Sidekiq::ClientMiddleware
+
+      def call(_worker_class, job, _queue, _redis_pool)
+        job["corral"] = Corral.current if Corral.current
+        job["queue"] = job["corral"] if job["corral"]
+        yield
+      end
+    end
+
+    class Server
+      include Sidekiq::ServerMiddleware
+
+      def call(_worker, job, _queue)
+        Corral.current = job["corral"] if job["corral"]
+        yield
+      ensure
+        Corral.current = nil
+      end
     end
   end
 end
--- test/sidekiq/test_corral.rb
+++ test/sidekiq/test_corral.rb
@@ -22,5 +22,78 @@ module Sidekiq
       Corral.current = "my_corral"
       assert_equal("my_corral", Corral.current)
     end
+
+    class ClientTest < Minitest::Test
+      def setup
+        @worker_class = DummyWorker
+        @job = { "args" => [1, 2, "buckle my shoe"], "queue" => "low" }
+        @redis_pool = "fake_redis_pool"
+      end
+
+      def teardown
+        Corral.current = nil
+      end
+
+      def test_updating_queue_and_job_corral_with_current_corral
+        assert_nil(@job["corral"])
+        Corral.current = "critical"
+        Sidekiq::Corral::Client
+          .new
+          .call(@worker_class, @job, @job["queue"], @redis_pool) { "next_step" }
+        assert_equal("critical", @job["corral"])
+        assert_equal("critical", @job["queue"])
+      end
+
+      def test_updating_queue_with_corral_in_job_payload
+        @job["corral"] = "critical"
+        assert_nil(Corral.current)
+        Sidekiq::Corral::Client
+          .new
+          .call(@worker_class, @job, @job["queue"], @redis_pool) { "next_step" }
+        assert_equal("critical", @job["queue"])
+      end
+
+      def test_no_corral
+        assert_nil(Corral.current)
+        Sidekiq::Corral::Client
+          .new
+          .call(@worker_class, @job, @job["queue"], @redis_pool) { "next_step" }
+        assert_nil(@job["corral"])
+        assert_equal("low", @job["queue"])
+      end
+    end
+
+    class ServerTest < Minitest::Test
+      def setup
+        @worker_class = DummyWorker
+        @job = { "args" => [1, 2, "buckle my shoe"], "queue" => "critical", "corral" => "critical" }
+      end
+
+      def test_setting_current_corral_during_execution
+        Sidekiq::Corral::Server
+          .new
+          .call(@worker_class, @job, @job["queue"]) { assert_equal(@job["corral"], Corral.current) }
+        assert_nil(Corral.current)
+      end
+
+      def test_clearing_current_corral_on_error
+        assert_raises(StandardError) do
+          Sidekiq::Corral::Server
+            .new
+            .call(@worker_class, @job, @job["queue"]) do
+              assert_equal(@job["corral"], Corral.current)
+              fail(StandardError, "job failure")
+            end
+        end
+        assert_nil(Corral.current)
+      end
+
+      def test_no_corral
+        @job.delete("corral")
+        Sidekiq::Corral::Server
+          .new
+          .call(@worker_class, @job, @job["queue"]) { assert_nil(Corral.current) }
+      end
+    end
   end
 end
